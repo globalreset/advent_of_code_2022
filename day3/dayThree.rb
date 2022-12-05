78caa7ebe65704fcd2e07e86002735beccfd8b54
globalreset <shughes@pobox.com>
2022-12-05 16:27:19 -0600

    some cleanup

--- day3/dayThree.rb
+++ day3/dayThree.rb
@@ -11,24 +11,15 @@ end
 
 common = []
 inputList.each { |i|
-   r1 = i[0...(i.size/2)]
-   r2 = i[(i.size/2)..-1]
-
-   r1.each { |j|
-      if(r2.index(j))
-         common << j
-         break
-      end
-   }
+   r1,r2 = i.each_slice(i.size/2).to_a
+   common << (r1 & r2)[0]
 }
 
-prio = common.map(&:ord).map{ |i| getPrio(i) }
-p prio.sum
+p common.map(&:ord).map{ |i| getPrio(i) }.sum
 
 badges = []
 inputList.each_slice(3) { |triplet|
    badges << (triplet[0] & triplet[1] & triplet[2])[0]
 }
 
-prio = badges.map(&:ord).map { |i| getPrio(i) }
-p prio.sum
+p badges.map(&:ord).map { |i| getPrio(i) }.sum
