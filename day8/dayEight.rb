70538d3310b2a8022b0e3b4e924430d36792e5f8
globalreset <shughes@pobox.com>
2022-12-07 23:37:16 -0600

    switch col/row variable names, I had them backwards and it was bugging me

--- day8/dayEight.rb
+++ day8/dayEight.rb
@@ -7,32 +7,32 @@ inputList.each { |line|
 
 visibleCnt = treeMap[0].size*2 + (treeMap.size - 2)*2
 
-(1...(treeMap[0].size-1)).each { |row|
-   (1...(treeMap.size-1)).each { |col|
+(1...(treeMap[0].size-1)).each { |col|
+   (1...(treeMap.size-1)).each { |row|
       visibleCnt += [
-         treeMap[col][(row+1)..-1].max,
-         treeMap[col][0..(row-1)].max,
-         treeMap[0..(col-1)].map { |c| c[row] }.max,
-         treeMap[(col+1)..-1].map { |c| c[row] }.max
-      ].map { |m| 1 if(treeMap[col][row] > m) }.compact.max || 0
+         treeMap[row][(col+1)..-1].max,
+         treeMap[row][0..(col-1)].max,
+         treeMap[0..(row-1)].map { |c| c[col] }.max,
+         treeMap[(row+1)..-1].map { |c| c[col] }.max
+      ].map { |m| 1 if(treeMap[row][col] > m) }.compact.max || 0
    }
 }
 
 p visibleCnt
 
 scenicScore = []
-(1...(treeMap[0].size-1)).each { |row|
-   (1...(treeMap.size-1)).each { |col|
+(1...(treeMap[0].size-1)).each { |col|
+   (1...(treeMap.size-1)).each { |row|
       scenicScore << [
-         treeMap[col][(row+1)..-1],
-         treeMap[col][0..(row-1)].reverse,
-         treeMap[0..(col-1)].map { |c| c[row] }.reverse,
-         treeMap[(col+1)..-1].map { |c| c[row] }
+         treeMap[row][(col+1)..-1],
+         treeMap[row][0..(col-1)].reverse,
+         treeMap[0..(row-1)].map { |c| c[col] }.reverse,
+         treeMap[(row+1)..-1].map { |c| c[col] }
       ].map { |vec|
          treeCnt = 0
          vec.each { |t|
             treeCnt += 1
-            if(t >= treeMap[col][row])
+            if(t >= treeMap[row][col])
                break
             end
          }
